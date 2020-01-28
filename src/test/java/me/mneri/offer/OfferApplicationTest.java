6eee6753465c6bb0b470035300b6fabcec34259c
Massimo Neri <hello@mneri.me>
2020-01-28 23:43:14 +0000

    Created OfferApplication

---
+++ src/test/java/me/mneri/offer/OfferApplicationTest.java
@@ -0,0 +1,11 @@
+package me.mneri.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferApplicationTest {
+    @Test
+    void contextLoad() {
+    }
+}
