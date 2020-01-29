def399ffc50f3500c97d102336a5d2f0a17c1018
Massimo Neri <hello@mneri.me>
2020-01-29 00:33:28 +0000

    Added UserRepository

---
+++ src/main/java/me/mneri/offer/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.mneri.offer.repository;
+
+import me.mneri.offer.entity.User;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<String, User>, JpaSpecificationExecutor<User> {
+}
