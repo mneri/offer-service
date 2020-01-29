d39ac4cc6b16316b652c3c74c1717a9ec4d3f1ed
Massimo Neri <hello@mneri.me>
2020-01-29 00:30:05 +0000

    Added Offer entity.

---
+++ src/main/java/me/mneri/offer/entity/Offer.java
@@ -0,0 +1,50 @@
+package me.mneri.offer.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+@AllArgsConstructor(access = PRIVATE)
+@Builder
+@Data
+@Entity
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@ToString
+public class Offer {
+    @Id
+    @NonNull
+    @Setter(PROTECTED)
+    private String id;
+
+    @Column
+    @NonNull
+    private String title;
+
+    @Column
+    @NonNull
+    private String description;
+
+    @Column
+    @NonNull
+    private Date startTime;
+
+    @Column
+    @NonNull
+    private Date endTime;
+
+    @Column
+    private boolean expired;
+
+    @ManyToOne(fetch = LAZY)
+    @NonNull
+    private User owner;
+}
