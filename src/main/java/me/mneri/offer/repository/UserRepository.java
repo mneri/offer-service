b31573d38881f241bb7513eb69c88d87e38a6e35
Massimo Neri <hello@mneri.me>
2020-01-29 00:46:10 +0000

    Corrected typo in UserRepository

--- src/main/java/me/mneri/offer/repository/UserRepository.java
+++ src/main/java/me/mneri/offer/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<String, User>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends CrudRepository<User, String>, JpaSpecificationExecutor<User> {
 }
