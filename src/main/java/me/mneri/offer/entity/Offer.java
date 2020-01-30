b29b440b77f15aabc954c044ffd3cdfc68b5525b
Massimo Neri <hello@mneri.me>
2020-01-30 09:23:36 +0000

    Missing Offer.java from the previous commit

--- src/main/java/me/mneri/offer/entity/Offer.java
+++ src/main/java/me/mneri/offer/entity/Offer.java
@@ -6,20 +6,31 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.UUID;
 
 import static javax.persistence.FetchType.LAZY;
-import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
-@AllArgsConstructor(access = PRIVATE)
-@Builder
 @Data
 @Entity
 @EqualsAndHashCode(of = "id")
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 @ToString
 public class Offer {
+    @Builder
+    private Offer(@NonNull String title, @NonNull String description, @NonNull BigDecimal price,
+                  @NonNull String currency, @NonNull Date end, @NonNull User publisher) {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.currency = currency;
+        this.end = end;
+        this.publisher = publisher;
+    }
+
     @Id
     @NonNull
     @Setter(PROTECTED)
@@ -33,18 +44,22 @@ public class Offer {
     @NonNull
     private String description;
 
-    @Column
+    @Column(precision = 16, scale = 2)
     @NonNull
-    private Date startTime;
+    private BigDecimal price;
 
     @Column
     @NonNull
-    private Date endTime;
+    private String currency;
+
+    @Column(name = "end_time")
+    @NonNull
+    private Date end;
 
     @Column
     private boolean canceled;
 
     @ManyToOne(fetch = LAZY)
     @NonNull
-    private User owner;
+    private User publisher;
 }
