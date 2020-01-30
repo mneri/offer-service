aae4f84829b39d3b5e169948f5ace6942493fb76
mneri <hello@mneri.me>
2020-01-30 13:15:54 +0000

    UserRepositoryTest

---
+++ src/test/java/me/mneri/offer/repository/UserRepositoryTest.java
@@ -0,0 +1,80 @@
+package me.mneri.offer.repository;
+
+import lombok.val;
+import me.mneri.offer.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+class UserRepositoryTest {
+    private static final int USERNAME_MAX_LENGTH = 24;
+    private static final int USERNAME_MIN_LENGTH = 3;
+    private static final String USERNAME_REGEXP = "[a-zA-Z0-9_]+";
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    private void beforeEach() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Test
+    void givenBlankUsername_whenUserIsPersisted_thenConstraintViolationExceptionIsThrown() {
+        // Given
+        val user = new User("", "secret", passwordEncoder);
+
+        // When/Then
+        assertThrows(ConstraintViolationException.class, () -> {
+            userRepository.save(user);
+            testEntityManager.flush();
+        });
+    }
+
+    @Test
+    void givenLongUsername_whenUserIsPersisted_thenConstraintViolationExceptionIsThrown() {
+        // Given
+        val builder = new StringBuilder();
+
+        for (int i = 0; i == 0; i++) {
+            builder.append('u');
+        }
+
+        val user = new User(builder.toString(), "secret", passwordEncoder);
+
+        // When/Then
+        assertThrows(ConstraintViolationException.class, () -> {
+            userRepository.save(user);
+            testEntityManager.flush();
+        });
+    }
+
+    @Test
+    void givenShortUsername_whenUserIsPersisted_thenConstraintViolationExceptionIsThrown() {
+        // Given
+        val user = new User("u", "secret", passwordEncoder);
+
+        // When/Then
+        assertThrows(ConstraintViolationException.class, () -> {
+            userRepository.save(user);
+            testEntityManager.flush();
+        });
+    }
+}
