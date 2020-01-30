fd21acd4a2413c134a0013a39429bb24e38b2003
Massimo Neri <hello@mneri.me>
2020-01-30 09:23:08 +0000

    OfferTest

---
+++ src/test/java/me/mneri/offer/entity/OfferTest.java
@@ -0,0 +1,54 @@
+package me.mneri.offer.entity;
+
+import lombok.val;
+import me.mneri.offer.util.TextUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static java.lang.System.currentTimeMillis;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class OfferTest {
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void beforeEach() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Test
+    void givenAllParameters_whenNewInstanceIsConstructed_thenAllFieldsAreProperlyInitialized() {
+        // Given
+        val title = "Bazinga";
+        val description = "Awesome";
+        val price = new BigDecimal("100.00");
+        val currency = "GBP";
+        val end = new Date(currentTimeMillis() + 30 * 24 * 60 * 60 * 1000L);
+        val publisher = new User("user", "secret", passwordEncoder);
+
+        // When
+        val instance = Offer.builder()
+                .title(title)
+                .description(description)
+                .price(price)
+                .currency(currency)
+                .end(end)
+                .publisher(publisher)
+                .build();
+
+        // Then
+        assertFalse(TextUtil.isEmpty(instance.getId()));
+        assertEquals(title, instance.getTitle());
+        assertEquals(description, instance.getDescription());
+        assertEquals(0, price.compareTo(instance.getPrice()));
+        assertEquals(currency, instance.getCurrency());
+        assertEquals(end, instance.getEnd());
+        assertEquals(publisher, instance.getPublisher());
+    }
+}
