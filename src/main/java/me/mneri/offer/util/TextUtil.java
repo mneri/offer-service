a3a1babbb314f424df2bfa895e75fd8a94aa0770
mneri <hello@mneri.me>
2020-01-30 13:06:12 +0000

    Added final modifier, documentation

--- src/main/java/me/mneri/offer/util/TextUtil.java
+++ src/main/java/me/mneri/offer/util/TextUtil.java
@@ -4,8 +4,19 @@ import lombok.NoArgsConstructor;
 
 import static lombok.AccessLevel.PRIVATE;
 
+/**
+ * Utility class for strings and text.
+ *
+ * @author mneri
+ */
 @NoArgsConstructor(access = PRIVATE)
-public class TextUtil {
+public final class TextUtil {
+    /**
+     * Return {@code true} if the string is {@code null} or empty, {@code false} otherwise.
+     *
+     * @param string The string to test.
+     * @return {@code true} if the string is {@code null} or empty, {@code false} otherwise.
+     */
     public static boolean isEmpty(String string) {
         return string == null || string.length() == 0;
     }
