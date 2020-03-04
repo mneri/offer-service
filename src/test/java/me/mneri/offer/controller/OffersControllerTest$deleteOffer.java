6d217d7ab19fc95b016cea441950f6d07d88c762
Massimo Neri <hello@mneri.me>
2020-03-04 23:35:22 +0000

    Copyright notice

--- src/test/java/me/mneri/offer/controller/OffersControllerTest$deleteOffer.java
+++ src/test/java/me/mneri/offer/controller/OffersControllerTest$deleteOffer.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2020 Massimo Neri <hello@mneri.me>
+ *
+ * This file is part of mneri/offer-service.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.mneri.offer.controller;
 
 import lombok.SneakyThrows;
@@ -10,7 +28,6 @@ import me.mneri.offer.exception.UserNotAuthorizedException;
 import me.mneri.offer.service.OfferService;
 import me.mneri.offer.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
